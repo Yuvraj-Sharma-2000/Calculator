64c7d75a5a704bf04f0ce315cbb2daffb613583a
Yuvraj Sharma <103098216+Yuvraj-Sharma-2000@users.noreply.github.com>
2023-03-13 22:24:35 +0530

    Update Main.java

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,72 +1,85 @@
 package org.example;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import java.util.Scanner;
 
 public class Main {
+    private static final Logger logger = LogManager.getLogger(Main.class);
     public static void main(String[] args) {
+        logger.info("Started Calculator Application");
         calculator();
+        logger.info("Exited Calculator Application");
     }
 
-public static void calculator() {
-    Scanner scanner = new Scanner(System.in);
-    int choice = 0;
+    public static void calculator() {
+        Scanner scanner = new Scanner(System.in);
+        int choice = 0;
 
-    do {
-        System.out.println("Scientific Calculator");
-        System.out.println("---------------------");
-        System.out.println("1. Square root function - √x");
-        System.out.println("2. Factorial function - x!");
-        System.out.println("3. Natural logarithm (base е) - ln(x)");
-        System.out.println("4. Power function - x^y");
-        System.out.println("0. Exit");
+        do {
+            System.out.println("Scientific Calculator");
+            System.out.println("---------------------");
+            System.out.println("1. Square root function - √x");
+            System.out.println("2. Factorial function - x!");
+            System.out.println("3. Natural logarithm (base е) - ln(x)");
+            System.out.println("4. Power function - x^y");
+            System.out.println("0. Exit");
 
-        System.out.print("Enter your choice: ");
-
-        // Check if there is more input to be read
-        if (scanner.hasNextInt()) {
+            System.out.print("Enter your choice: ");
             choice = scanner.nextInt();
 
             switch(choice) {
                 case 1:
+                    logger.info("Operation: SQUARE ROOT");
                     System.out.print("Enter a number: ");
                     double squareRootNumber = scanner.nextDouble();
+                    logger.info("Number entered: "+squareRootNumber);
                     double squareRootResult = getSqrt(squareRootNumber);
                     System.out.println("The square root of " + squareRootNumber + " is " + squareRootResult);
+                    logger.info("Got result: "+squareRootResult);
                     break;
                 case 2:
+                    logger.info("Operation: SQUARE ROOT");
                     System.out.print("Enter a number: ");
                     int factorialNumber = scanner.nextInt();
+                    logger.info("Number entered: "+factorialNumber);
                     int factorialResult = getFactorial(factorialNumber);
                     System.out.println(factorialNumber + "! = " + factorialResult);
+                    logger.info("Got result: "+factorialResult);
                     break;
                 case 3:
+                    logger.info("Operation: SQUARE ROOT");
                     System.out.print("Enter a number: ");
                     double naturalLogNumber = scanner.nextDouble();
+                    logger.info("Number entered: "+naturalLogNumber);
                     double naturalLogResult = getLog(naturalLogNumber);
                     System.out.println("The natural logarithm of " + naturalLogNumber + " is " + naturalLogResult);
+                    logger.info("Got result: "+naturalLogResult);
                     break;
                 case 4:
+                    logger.info("Operation: SQUARE ROOT");
                     System.out.print("Enter the base: ");
                     double powerBase = scanner.nextDouble();
+                    logger.info("Base entered: "+powerBase);
                     System.out.print("Enter the exponent: ");
                     double powerExponent = scanner.nextDouble();
+                    logger.info("Power entered: "+powerExponent);
                     double powerResult = getPow(powerBase, powerExponent);
                     System.out.println(powerBase + "^" + powerExponent + " = " + powerResult);
+                    logger.info("Got result: "+powerResult);
                     break;
                 case 0:
+                    logger.info("Exit");
                     System.out.println("Exiting...");
                     break;
                 default:
+                    logger.info("Invalid choice");
                     System.out.println("Invalid choice. Please try again.");
             }
-        } else {
-            // If there is no more input, break out of the loop
-            break;
-        }
-    } while(choice != 0);
+        } while(choice != 0);
 
-    scanner.close();
-}
+        scanner.close();
+    }
 
     public static double getPow(double powerBase, double powerExponent) {
         return Math.pow(powerBase, powerExponent);
